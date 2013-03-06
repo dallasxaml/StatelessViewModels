01cf1b413cbf4fa731e4e696c9e5f604bea79a08
Michael L Perry <michael@qedcode.com>
2013-03-05 19:03:35 -0600

    Data bind to main view model.

---
+++ StatelessViewModels/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpdateControls.XAML;
+using StatelessViewModels.Models;
+
+namespace StatelessViewModels.ViewModels
+{
+    public class ViewModelLocator : ViewModelLocatorBase
+    {
+        private Document _document;
+		private Selection _selection;
+
+        public ViewModelLocator()
+        {
+            _document = LoadDocument();
+			_selection = new Selection();
+        }
+
+        public object Main
+        {
+            get { return ViewModel(() => new MainViewModel(_document, _selection)); }
+        }
+
+		public object Item
+		{
+			get
+			{
+				return ViewModel(() => _selection.SelectedItem == null
+					? null
+					: new ItemViewModel(_selection.SelectedItem));
+			}
+		}
+
+		private Document LoadDocument()
+		{
+			// TODO: Load your document here.
+            Document document = new Document();
+            var one = document.NewItem();
+            one.Name = "One";
+            var two = document.NewItem();
+            two.Name = "Two";
+            var three = document.NewItem();
+            three.Name = "Three";
+            return document;
+		}
+    }
+}
